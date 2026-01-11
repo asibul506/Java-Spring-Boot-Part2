22da3fbed83f43666e6c184c24e4884170d7731d
Asibul Hasan Fahim <62127427+asibul506@users.noreply.github.com>
2026-01-11 18:12:01 +0100

    Validating business rules

--- src/main/java/com/codewithmosh/store/repositories/UserRepository.java
+++ src/main/java/com/codewithmosh/store/repositories/UserRepository.java
@@ -1,8 +1,11 @@
 package com.codewithmosh.store.repositories;
 
 import com.codewithmosh.store.entities.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail( String email);
 }
