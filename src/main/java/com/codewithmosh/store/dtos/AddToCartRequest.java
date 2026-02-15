61de66fe534ab7fbaa056b3f83e65aa7af43961b
Asibul Hasan Fahim <62127427+asibul506@users.noreply.github.com>
2026-02-15 03:36:00 +0100

    Adding a product to a Cart

---
+++ src/main/java/com/codewithmosh/store/dtos/AddToCartRequest.java
@@ -0,0 +1,11 @@
+package com.codewithmosh.store.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class AddToCartRequest {
+    @NotNull(message = "Product ID is required")
+    private Long productId;
+}
