5626f808412fc5e33e5b7c835b4df27a271108a5
Asibul Hasan Fahim <62127427+asibul506@users.noreply.github.com>
2026-02-16 01:19:14 +0100

    API documentation using Swagger

--- src/main/java/com/codewithmosh/store/controller/CartController.java
+++ src/main/java/com/codewithmosh/store/controller/CartController.java
@@ -7,6 +7,9 @@ import com.codewithmosh.store.dtos.UpdateCartItemRequest;
 import com.codewithmosh.store.exceptions.CartNotFoundException;
 import com.codewithmosh.store.exceptions.ProductNotFoundException;
 import com.codewithmosh.store.services.CartServices;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,10 +23,12 @@ import java.util.UUID;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/carts")
+@Tag(name = "Cart API", description = "Operations related to shopping carts") // Swagger annotation for API documentation
 public class CartController {
     private CartServices cartServices;
 
     @PostMapping
+    @Operation(summary = "Create a new shopping cart", description = "Creates a new shopping cart and returns its details") // Swagger annotation for method documentation
     public ResponseEntity<CartDto> createCart(UriComponentsBuilder uriBuilder) {
         var cartDto = cartServices.createCart();
         var uri = uriBuilder.path("/carts/{id}").buildAndExpand(cartDto.getId()).toUri();
@@ -32,7 +37,11 @@ public class CartController {
     }
 
     @PostMapping("/{cartId}/items")
-    public ResponseEntity<CartItemDto> addToCart(@PathVariable UUID cartId, @Valid @RequestBody AddToCartRequest request) {
+    public ResponseEntity<CartItemDto> addToCart(
+            @Parameter(description = "ID of the cart to which the product will be added", example = "123e4567-e89b-12d3-a456-426614174000") // Swagger annotation for parameter documentation
+            @PathVariable UUID cartId,
+            @Valid @RequestBody AddToCartRequest request
+    ) {
         var cartItemDto = cartServices.addToCart(cartId, request.getProductId());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(cartItemDto);
