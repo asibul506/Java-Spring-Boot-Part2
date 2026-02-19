5910883aa4a83246c12bc33db0a51b989e6ee5d5
Asibul Hasan Fahim <62127427+asibul506@users.noreply.github.com>
2026-02-19 22:42:30 +0100

    Hashing Passwords

--- src/main/java/com/codewithmosh/store/config/SecurityConfig.java
+++ src/main/java/com/codewithmosh/store/config/SecurityConfig.java
@@ -7,12 +7,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    // This bean is used to encode passwords before storing them in the database. It uses the BCrypt hashing algorithm, which is a strong and secure way to hash passwords.
+    // By defining this bean, we can inject it into our services or controllers where we need to encode passwords, ensuring that user passwords are stored securely.
+    // Additionally, Spring Security requires a PasswordEncoder bean to be defined in order to handle password encoding and matching during authentication.
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // This bean configures the security filter chain for the application. It defines how HTTP requests are secured and what kind of authentication is required for different endpoints.
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
